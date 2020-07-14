d71dde610828601539b8849b998c33b586849a82
Ankur Saha <ankursahamoto@gmail.com>
2020-07-14 09:14:57 -0800

    my anime

---
+++ animemovie.js
@@ -0,0 +1,209 @@
+ var x=1
+   $("#kimi-button").click(function()
+   {
+       if(($("#kimi-button").html())=="Read More")
+       {
+            $("#dotskimi").css("display","none");
+            $("#morekimi").css("display","block");
+
+       }
+       if(($("#kimi-button").html())=="Read Less")
+       {
+            $("#dotskimi").css("display","block");
+            $("#morekimi").css("display","none");
+
+       }
+         if(x%2!=0)
+            {
+                 $("#kimi-button").html("Read Less");
+                 x=x+1;
+            }
+           else
+           {
+                 $("#kimi-button").html("Read More");
+                 x=x+1;
+           }
+
+
+   });
+   var hello=1
+    $("#hello-button").click(function()
+   {
+       if(($("#hello-button").html())=="Read More")
+       {
+            $("#dotshello").css("display","none");
+            $("#morehello").css("display","block");
+
+       }
+       if(($("#hello-button").html())=="Read Less")
+       {
+            $("#dotshello").css("display","block");
+            $("#morehello").css("display","none");
+
+       }
+         if(hello%2!=0)
+            {
+                 $("#hello-button").html("Read Less");
+                 hello=hello+1;
+            }
+           else
+           {
+                 $("#hello-button").html("Read More");
+                 hello=hello+1;
+           }
+
+
+   });
+
+
+   var weather=1
+    $("#weather-button").click(function()
+   {
+       if(($("#weather-button").html())=="Read More")
+       {
+            $("#dotsweather").css("display","none");
+            $("#moreweather").css("display","block");
+
+       }
+       if(($("#weather-button").html())=="Read Less")
+       {
+            $("#dotsweather").css("display","block");
+            $("#moreweather").css("display","none");
+
+       }
+         if(weather%2!=0)
+            {
+                 $("#weather-button").html("Read Less");
+                 weather=weather+1;
+            }
+           else
+           {
+                 $("#weather-button").html("Read More");
+                 weather=weather+1;
+           }
+
+
+   });
+
+
+    var maquia=1
+    $("#maquia-button").click(function()
+   {
+       if(($("#maquia-button").html())=="Read More")
+       {
+            $("#dotsmaquia").css("display","none");
+            $("#moremaquia").css("display","block");
+
+       }
+       if(($("#maquia-button").html())=="Read Less")
+       {
+            $("#dotsmaquia").css("display","block");
+            $("#moremaquia").css("display","none");
+
+       }
+         if(maquia%2!=0)
+            {
+                 $("#maquia-button").html("Read Less");
+                 maquia=maquia+1;
+            }
+           else
+           {
+                 $("#maquia-button").html("Read More");
+                  maquia=maquia+1;
+           }
+
+
+   });
+
+
+    var wolf=1
+    $("#wolf-button").click(function()
+   {
+       if(($("#wolf-button").html())=="Read More")
+       {
+            $("#dotswolf").css("display","none");
+            $("#morewolf").css("display","block");
+
+       }
+       if(($("#wolf-button").html())=="Read Less")
+       {
+            $("#dotswolf").css("display","block");
+            $("#morewolf").css("display","none");
+
+       }
+         if(wolf%2!=0)
+            {
+                 $("#wolf-button").html("Read Less");
+               wolf=wolf+1;
+            }
+           else
+           {
+                 $("#wolf-button").html("Read More");
+                 wolf=wolf+1;
+           }
+
+
+   });
+
+
+
+    var blue=1
+    $("#blue-button").click(function()
+   {
+       if(($("#blue-button").html())=="Read More")
+       {
+            $("#dotsblue").css("display","none");
+            $("#moreblue").css("display","block");
+
+       }
+       if(($("#blue-button").html())=="Read Less")
+       {
+            $("#dotsblue").css("display","block");
+            $("#moreblue").css("display","none");
+
+       }
+         if(blue%2!=0)
+            {
+                 $("#blue-button").html("Read Less");
+              blue=blue+1;
+            }
+           else
+           {
+                 $("#blue-button").html("Read More");
+                 blue=blue+1;
+           }
+
+
+   });
+
+
+
+    var voice=1
+    $("#voice-button").click(function()
+   {
+       if(($("#voice-button").html())=="Read More")
+       {
+            $("#dotsvoice").css("display","none");
+            $("#morevoice").css("display","block");
+
+       }
+       if(($("#voice-button").html())=="Read Less")
+       {
+            $("#dotsvoice").css("display","block");
+            $("#morevoice").css("display","none");
+
+       }
+         if(voice%2!=0)
+            {
+                 $("#voice-button").html("Read Less");
+             voice=voice+1
+            }
+           else
+           {
+                 $("#voice-button").html("Read More");
+                  voice=voice+1
+           }
+
+
+   });
+
